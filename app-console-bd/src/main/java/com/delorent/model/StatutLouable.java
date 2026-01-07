da92c5c84e8c9940fec1c5a481c8ecd99d04e168
Sacha <brezsacha@gmail.com>
2026-01-07 17:17:19 +0100

    Modele Louable
    base louable

---
+++ app-console-bd/src/main/java/com/delorent/model/StatutLouable.java
@@ -0,0 +1,8 @@
+package com.delorent.model;
+
+public enum StatutLouable {
+    DISPONIBLE,
+    INDISPONIBLE,
+    EN_LOCATION,
+    EN_ENTRETIEN
+}
---
+++ app-console-bd/src/main/java/com/delorent/model/TypeMoto.java
@@ -0,0 +1,9 @@
+package com.delorent.model;
+
+public enum TypeMoto {
+    ROADSTER,
+    SPORTIVE,
+    SCOOTER,
+    CUSTOM,
+    TRAIL
+}
