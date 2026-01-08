58ac275c1063584d9891356978d003b80edbb7fb
kunail0804 <kunail0804@gmail.com>
2026-01-08 13:06:10 +0100

    Agent extends Utilisateur

--- app-console-bd/src/main/java/com/delorent/model/Agent.java
+++ app-console-bd/src/main/java/com/delorent/model/Agent.java
@@ -1,18 +1,19 @@
 package com.delorent.model;
 
-public class Agent {
+public class Agent extends Utilisateur{
 
    private String typeAgent;
    private String nom;
    private String prenom;
 
-   public Agent (String typeAgent, String nom, String prenom)
-       {
-       this.typeAgent = typeAgent;
-       this.nom = nom;
-       this.prenom = prenom;
-       }
-
+   public Agent(int idUtilisateur, String adresse, String ville, String codePostal, String region, String telephone,
+      String mail, String motDePasse, String typeAgent, String nom, String prenom) {
+    super(idUtilisateur, adresse, ville, codePostal, region, telephone, mail, motDePasse);
+    this.typeAgent = typeAgent;
+    this.nom = nom;
+    this.prenom = prenom;
+  }
+  
    public String getTypeAgent () { return typeAgent; }
    public String getNom () { return nom; }
    public String getPrenom () { return prenom; }
