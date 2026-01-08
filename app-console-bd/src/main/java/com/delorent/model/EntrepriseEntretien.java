8779afc7ad4e2c11e0e6a50698e705f1af35a6ad
OliveNabia <Olivenabia7@gmail.com>
2026-01-08 13:00:47 +0100

    Creation d'utilisateurs dans Model

---
+++ app-console-bd/src/main/java/com/delorent/model/EntrepriseEntretien.java
@@ -0,0 +1,53 @@
+package com.delorent.model;
+
+public class EntrepriseEntretien extends Utilisateur {
+
+    private String nomEntreprise;
+    private String raisonSoc;
+    private String noSiret;
+
+    public EntrepriseEntretien(
+            int idUtilisateur,
+            String adresse,
+            String ville,
+            String codePostal,
+            String region,
+            String telephone,
+            String mail,
+            String motDePasse,
+            String nomEntreprise,
+            String raisonSoc,
+            String noSiret
+    ) {
+        super(idUtilisateur, adresse, ville, codePostal, region,
+              telephone, mail, motDePasse);
+
+        this.nomEntreprise = nomEntreprise;
+        this.raisonSoc = raisonSoc;
+        this.noSiret = noSiret;
+    }
+
+    public String getNomEntreprise() {
+        return nomEntreprise;
+    }
+
+    public String getRaisonSoc() {
+        return raisonSoc;
+    }
+
+    public String getNoSiret() {
+        return noSiret;
+    }
+
+    public void setNomEntreprise(String nomEntreprise) {
+        this.nomEntreprise = nomEntreprise;
+    }
+
+    public void setRaisonSoc(String raisonSoc) {
+        this.raisonSoc = raisonSoc;
+    }
+
+    public void setNoSiret(String noSiret) {
+        this.noSiret = noSiret;
+    }
+}
