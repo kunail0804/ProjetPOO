110c48e0e9f1c6f7642c9b807b21bfa39fa6795e
OliveNabia <Olivenabia7@gmail.com>
2026-01-08 13:00:47 +0100

    Creation d'utilisateurs dans Model

---
+++ app-console-bd/src/main/java/com/delorent/model/Utilisateur.java
@@ -0,0 +1,109 @@
+package com.delorent.model;
+
+
+//import java.time.LocalDate;
+
+public class Utilisateur {
+
+   private int idUtilisateur;
+   private String adresse;
+    private String ville;
+    private String codePostal;
+    private String region;
+    private String telephone;
+    private String mail;
+    private String motDePasse;
+    //private LocalDate dateInscription;
+
+    // Constructeur
+    public Utilisateur(int idUtilisateur,  String adresse,
+                       String ville, String codePostal, String region,
+                       String telephone, String mail, String motDePasse
+                       /*LocalDate dateInscription*/) {
+        this.idUtilisateur = idUtilisateur;
+        this.adresse = adresse;
+        this.ville = ville;
+        this.codePostal = codePostal;
+        this.region = region;
+        this.telephone = telephone;
+        this.mail = mail;
+        this.motDePasse = motDePasse;
+        //this.dateInscription = dateInscription;
+    }
+
+    // Getters et Setters
+
+    public int getIdUtilisateur() {
+        return idUtilisateur;
+    }
+
+    public void setIdUtilisateur(int idUtilisateur) {
+        this.idUtilisateur = idUtilisateur;
+    }
+
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    /*public LocalDate getDateInscription() {
+        return dateInscription;
+    }
+
+    public void setDateInscription(LocalDate dateInscription) {
+        this.dateInscription = dateInscription;
+    }*/
+
+}
