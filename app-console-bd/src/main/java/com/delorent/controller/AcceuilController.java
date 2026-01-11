42f26dc5d61068181c84d3223f6a205eddacce86
kunail0804 <kunail0804@gmail.com>
2026-01-11 12:15:41 +0100

    v1 page d'accueil

--- app-console-bd/src/main/java/com/delorent/controller/AcceuilController.java
+++ app-console-bd/src/main/java/com/delorent/controller/AcceuilController.java
@@ -1,10 +1,22 @@
 package com.delorent.controller;
+
+import com.delorent.service.ConnexionService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AcceuilController {
-    public void redirigerVersRecherche(){
-        return;
+    private final ConnexionService connexionService;
+
+    public AcceuilController(ConnexionService connexionService) {
+        this.connexionService = connexionService;
     }
-    
-    public void redirigerVersProfil(){
-        return;
+
+    @GetMapping({"/", "/accueil"})
+    public String accueil(Model model) {
+        model.addAttribute("estConnecte", connexionService.estConnecte());
+        model.addAttribute("utilisateur", connexionService.getConnexion()); // optionnel (afficher nom/role)
+        return "accueil";
     }
 }
